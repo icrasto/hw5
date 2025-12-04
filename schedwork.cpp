0057b3407bb0ea5d9bca3bda08ba6c8324d00a82
Isabella Crasto <crasto@usc.edu>
2025-12-04 09:23:29 +0000

    submitting hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -1,6 +1,5 @@
 #ifndef RECCHECK
 #include <set>
-#include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
@@ -21,7 +20,10 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool helperFunc (const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched, size_t currentDay, size_t currentOpening, vector<size_t>& shiftsUsed);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -32,14 +34,78 @@ bool schedule(
     DailySchedule& sched
 )
 {
+
+    // NOTES: need to recurse through 0 -> n-1 days. + within each day, there are slots 0->d-1 that need to be filled
+    // for each recursive position, try each possible worker (index 0 -> k-1)
+    // need to track how many shifts have been worked so far for each worker -> use array? -> vector easier
+    
     if(avail.size() == 0U){
         return false;
     }
     sched.clear();
     // Add your code below
 
+    if (dailyNeed == 0 || maxShifts == 0) {
+        return false;
+    }
+    // sched is a n-by-d matrix with the d worker IDs who are scheduled to work on each of the n days
+    // creating vector with size being the amount of workers - all entries set to 0 to start
+    vector<size_t> shiftsUsed(avail[0].size(), 0);
 
+    // # of workers: avail[0].size()
+    // # of days: avail.size()
+    //  ^^^ avail is a n by k matrix. k workers availability over n day period
 
+    // adds rows to schedule (# of days) bc will be accessing them later to add worker IDs. even tho empty this is necessary
+    sched.resize(avail.size());
+    return helperFunc(avail, dailyNeed, maxShifts, sched, 0, 0, shiftsUsed);
 
 }
+
+bool helperFunc (const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched, size_t currentDay, size_t currentOpening, vector<size_t>& shiftsUsed) {
+        size_t numdays = avail.size();
+        size_t numworkers = avail[0].size();
+
+        // base case: completed all days in the matrix 
+        if (currentDay >= numdays) {
+            return true;
+        }
+        // @ the max opening for the row and need to move to the next day 
+        if (currentOpening >= dailyNeed) {
+            return helperFunc(avail, dailyNeed, maxShifts, sched, currentDay+1, 0, shiftsUsed);
+        }
+
+        // looping through all workers bc need to figure out which ones are available for current day and shift
+        for (size_t i = 0; i < numworkers; i++) {
+            // check if worker is available, hasnt used up all shifts
+            if (avail[currentDay][i] == true && shiftsUsed[i] < maxShifts) {
+                // also check if worker has been scheduled for this day already
+                bool scheduled = false;
+                for (size_t j = 0; j< sched[currentDay].size(); j++) {
+                    if (sched[currentDay][j] == i) {
+                        scheduled = true;
+                        break;
+                    }
+                }
+                if (scheduled == false) {
+                    // we are filling schedule n (days) by d (# of workers needed per day)
+                    sched[currentDay].push_back(i);
+                    shiftsUsed[i]++;
+                    if (helperFunc(avail, dailyNeed, maxShifts, sched, currentDay, currentOpening+1, shiftsUsed) == true) {
+                        return true;
+                    } else {
+                        // backtracking work
+                        shiftsUsed[i]--;
+                        sched[currentDay].pop_back();
+                    }
+                }
+            }
+            
+        }
+        // if made out entire for loop, that means no worker w availability was found
+            return false;
+    }
 
--- wordle.cpp
+++ wordle.cpp
@@ -1,6 +1,5 @@
 #ifndef RECCHECK
 // For debugging
-#include <iostream>
 // For std::remove
 #include <algorithm> 
 #include <map>
@@ -11,8 +10,59 @@
 #include "dict-eng.h"
 using namespace std;
 
+// OLD code:
+// then add to current word
+        // currentWord[pos] = in[pos];
+        // bool decreasedCount = false;
+        // int localYellowsLeft = yellowsLeft;
+        // decrease occurrence from array of yellow letters **check if there is overlap??
+        // if (yellowCounts[in[pos] - 'a'] > 0) {
+        //     yellowCounts[in[pos]-'a'] = yellowCounts[in[pos]-'a'] - 1;
+        //     localYellowsLeft--;
+        //     decreasedCount = true;
+        // }
+
+        // check if this combination is possible with the såpots left
+            // int spotsLeft = in.length() - (pos);
+            // int yellowsLeft = 0;
+
+            // std::map<char,int>::iterator it = yellowCounts.begin();
+            // while (it != yellowCounts.end()) {
+            //     yellowsLeft += it->second;
+            //     it++;
+            // }
+        
+
+        // go to the next position in recursion only if enough spots left for yellows
+        // if (localYellowsLeft <= spotsLeft) {
+
+
+        // }
+        // if (decreasedCount == true) {
+        //     yellowCounts[in[pos]-'a'] = yellowCounts[in[pos]-'a'] + 1;
+        //     // yellowsLeft++;
+        // }
+
+        // int yellowsLeft = 0;
+        // std::map<char,int>::iterator it = yellowCounts.begin();
+        // while (it != yellowCounts.end()) {
+        //     yellowsLeft += it->second;
+        //     it++;
+        // }
+
+        // if (yellowsLeft > spotsLeft) {
+            //     // not possible go to next letter
+            //     if (decreasedCount == true) {
+            //         yellowCounts[letter] = yellowCounts[letter] + 1;
+            //         yellowsLeft++;
+            //     }
+            //     letter++;
+            //     continue;
+            // }
+
 
 // Add prototypes of helper functions here
+void helperFunc(size_t pos, string& currentWord, size_t yellowCounts[26], size_t yellowsLeft, const std::string& in, const std::set<std::string>& dict, std::set<std::string>& results);
 
 
 // Definition of primary wordle function
@@ -23,6 +73,85 @@ std::set<std::string> wordle(
 {
     // Add your code here
 
+    std::set<std::string> results;
+    // empty case
+    if (in.length() == 0) {
+        return results;
+    }
+
+    // using array to store counts of floating letters. 26 size for each letter
+    // used map before but longer lookup
+    size_t yellowCounts[26]={0};
+    
+    for (size_t i=0; i < floating.length(); i++) {
+        char letter = floating[i];
+        yellowCounts[letter - 'a']++;
+    }
+
+    
+
+    // setting string of individual words created - set to length of in (final word length)
+    std::string currentWord = in;
+    int yellowsLeft = floating.length();
+    helperFunc(0, currentWord, yellowCounts, yellowsLeft, in, dict, results);
+    return results;
+
 }
 
 // Define any helper functions here
+void helperFunc(size_t pos, string& currentWord, size_t yellowCounts[26], size_t yellowsLeft, const std::string& in, const std::set<std::string>& dict, std::set<std::string>& results) {
+    
+    if (pos == in.length()) {
+        // make sure all yellow letters have been used & check if english word
+        if (yellowsLeft == 0 && dict.find(currentWord) != dict.end()) {
+            
+          results.insert(currentWord);
+        
+        }
+        return;
+    }
+    size_t spotsLeft = in.length() - (pos);
+    if (yellowsLeft > spotsLeft) {
+        return;
+    }
+    // then there is a letter (not a dash) position is predetermined
+    if (in[pos] != '-'){
+        
+            helperFunc(pos+1, currentWord, yellowCounts, yellowsLeft, in, dict, results);
+    } else {
+        // is a dash
+
+        // calculating spots left to compare inside while loop
+        
+    
+        char letter = 'a';
+        while (letter <= 'z') {
+            
+            currentWord[pos] = letter;
+            // decrease yellow letters count if needed
+            bool decreasedCount = false;
+            size_t localYellowsLeft = yellowsLeft;
+            if (yellowCounts[letter - 'a'] > 0) {
+                yellowCounts[letter - 'a']--;
+                localYellowsLeft--;
+                decreasedCount = true;
+            }
+            // check if this combination is possible with the spots left
+            
+            
+
+            // continue recursing through other letter combinations if spots left
+            if (localYellowsLeft <= spotsLeft-1) {
+                helperFunc(pos+1, currentWord, yellowCounts, localYellowsLeft, in, dict, results);
+            }
+            // backtracking
+            currentWord[pos] = '-';
+            if (decreasedCount == true) {
+                yellowCounts[letter - 'a']++;
+                // yellowsLeft++;
+            }
+            letter++;
+        }
+    }
+    
+}
